e506ebda3d10ec81a5bbadef798ad45d5bd6d4b6
marcosgabriel-junior <marcosguabrie@gmail.com>
2025-07-04 10:57:08 -0300

    Update _firebaseconfig.js

--- app/services/_firebaseconfig.js
+++ app/services/_firebaseconfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Esta é a variável correta com suas credenciais. O nome é "firebaseConfig".
 const firebaseConfig = {
-  apiKey: "AIzaSyD6-vCJZNOegSLJXtW3cgxd66XUrUsk8-Q",
-  authDomain: "vetfacil-ec010.firebaseapp.com",
-  projectId: "vetfacil-ec010",
-  storageBucket: "vetfacil-ec010.appspot.com",
-  messagingSenderId: "335392990884",
-  appId: "1:335392990884:web:76b6c1071198cebbfea831",
-  measurementId: "G-JKYS6TEGG8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: ",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // A função initializeApp usa a variável "firebaseConfig" (sem underscore).
